6933f7a9506608562710bc9a5dd5b5bf84d13467
pinheiro.gabriel@escolar.ifrn.edu.br <pinheiro.gabriel@escolar.ifrn.edu.br>
2026-07-16 17:44:19 -0300

    menu

---
+++ menu.py
@@ -0,0 +1,10 @@
+class Menu:
+    """Menu interativo para gerenciar objetos da academia em tempo real."""
+    def __init__(self, aluno, personal, gerente, filial, rede, treino, exercicio):
+        self.aluno = aluno
+        self.personal = personal
+        self.gerente = gerente
+        self.filial = filial
+        self.rede = rede
+        self.treino = treino
+        self.exercicio = exercicio
